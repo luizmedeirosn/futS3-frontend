81816b45367fd04ac6dc2fc7a0e1f53f7a6aca72
luizmedeirosn <netomedeirosdev@gmail.com>
2023-10-15 16:17:11 -0300

    feat: findAllPlayers method

---
+++ src/app/models/interfaces/player/response/FindAllPlayers.ts
@@ -0,0 +1,6 @@
+export interface FindAllPlayers {
+    playerId: number;
+    playerName: string;
+    positionId: number;
+    positionName: string;
+}
---
+++ src/app/services/player/player.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/app/environments/environment.prod';
+import { FindAllPlayers } from 'src/app/models/interfaces/player/response/FindAllPlayers';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PlayerService {
+
+    private API_URL: string = environment.API_URL;
+    private httpOptions = {
+
+    };
+
+    constructor (
+        private httpClient: HttpClient
+    ) {
+    }
+
+    public findAllPlayers(): Observable<Array<FindAllPlayers>> {
+        return this.httpClient.get<Array<FindAllPlayers>> (
+            `${this.API_URL}/players`,
+            this.httpOptions
+        );
+    }
+}
