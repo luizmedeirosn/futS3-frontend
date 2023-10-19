0a549bdedb662d451d4ab41cb39ebd5c57166685
luizmedeirosn <netomedeirosdev@gmail.com>
2023-10-18 23:34:04 -0300

    feat: applying the shorten pipe to the gamemode description

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,14 @@ import { MenubarModule } from 'primeng/menubar';
 import { ButtonModule } from 'primeng/button';
 
 import { MenubarNavigationComponent } from './components/menubar-navigation/menubar-navigation.component';
+import { ShortenPipe } from './pipes/shorten.pipe';
 
 
 
 @NgModule({
   declarations: [
-    MenubarNavigationComponent
+    MenubarNavigationComponent,
+    ShortenPipe
   ],
   imports: [
     CommonModule,
@@ -18,6 +20,6 @@ import { MenubarNavigationComponent } from './components/menubar-navigation/menu
     MenubarModule,
     ButtonModule
   ],
-  exports: [MenubarNavigationComponent]
+  exports: [MenubarNavigationComponent , ShortenPipe]
 })
 export class SharedModule { }
