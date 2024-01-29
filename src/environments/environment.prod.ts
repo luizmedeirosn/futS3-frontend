5124f83d041d2f4f90b7db6d06d94afabc37aad3
netomedeirosdev <netomedeirosdev@gmail.com>
2024-01-29 20:37:26 -0300

    Remove build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
+    // API_URL: 'http://18.231.176.75'
     API_URL: 'http://localhost:8080'
-    // API_URL: 'http://ec2-15-228-201-103.sa-east-1.compute.amazonaws.com'
 }
