a1bfaf659a235c9951fa3627f688938843434df4
luizmedeirosn <netomedeirosdev@gmail.com>
2024-03-11 16:16:53 -0300

    Fix refresh token request in the interceptor

--- src/app/interceptors/auth/auth.interceptor.ts
+++ src/app/interceptors/auth/auth.interceptor.ts
@@ -32,8 +32,8 @@ export class AuthInterceptor implements HttpInterceptor {
         AuthInterceptor.refreshToken = this.cookieService.get('_refreshToken');
     }
 
-    public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-        if (request.url === `${this.API_URL}/auth/signin`) {
+    public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (request.url === `${this.API_URL}/auth/signin` || request.url === `${this.API_URL}/auth/refresh-token`) {
             return next.handle(request);
         }
 
@@ -43,33 +43,41 @@ export class AuthInterceptor implements HttpInterceptor {
             }
         });
 
-        return next.handle(clone).pipe(catchError((err: HttpErrorResponse) => {
-            if (err.status === 403 && !this.refresh) {
-                this.refresh = true;
+        return next.handle(clone).pipe(
+            catchError((err: HttpErrorResponse) => {
+                if (err.status === 403 && !this.refresh) {
+                    this.refresh = true;
 
-                return this.http.put(`${this.API_URL}/auth/refresh-token`, {}, {
-                    headers: new HttpHeaders({
-                        Authorization: `Bearer ${AuthInterceptor.refreshToken}`
+                    return this.http.put(`${this.API_URL}/auth/refresh-token`, {}, {
+                        headers: new HttpHeaders({
+                            Authorization: `Bearer ${AuthInterceptor.refreshToken}`
+                        })
                     })
-                }).pipe(
-                    switchMap((response: any) => {
-                        AuthInterceptor.accessToken = response.accessToken;
+                    .pipe(
+                        switchMap((response: any) => {
+                            this.cookieService.set('_accessToken', response.accessToken);
+                            AuthInterceptor.accessToken = response.accessToken;
 
-                        return next.handle(clone.clone({
-                            setHeaders: {
-                                Authorization: `Bearer ${AuthInterceptor.accessToken}`
-                            }
-                        }));
-                    }),
-                    catchError((refreshErr: any) => {
-                        this.refresh = false;
-                        this.authService.logout(true);
-                        return throwError(() => refreshErr);
-                    })
-                );
+                            return next.handle(
+                                request.clone({
+                                    setHeaders: {
+                                        Authorization: `Bearer ${AuthInterceptor.accessToken}`
+                                    }
+                                })
+                            );
+                        }),
+                        catchError((refreshErr: any) => {
+                            this.refresh = false;
+                            this.authService.logout(true);
+                            return throwError(() => refreshErr);
+                        })
+                    );
+                    
+                }
+
+                this.refresh = false;
+                return throwError(() => err);
             }
-            this.refresh = false;
-            return throwError(() => err);
-        }));
+        ));
     }
 }
