20f76784c5cc06a2aeed5428ce237ec0c73774d1
luizmedeirosn <netomedeirosdev@gmail.com>
2024-04-20 23:05:33 -0300

    Style caret color transparent for menubar and buttons

--- src/app/models/events/ViewAction.ts
+++ src/app/models/events/ViewAction.ts
@@ -1,9 +1,6 @@
-import {TableLazyLoadEvent} from "primeng/table";
-
 export interface ViewAction {
 
     id: number;
     name?: string;
     description?: string;
-    tableLazyLoadEventPreview?: TableLazyLoadEvent;
 }
